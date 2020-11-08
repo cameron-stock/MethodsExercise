0aaa421ed4690c6cb6712059b986a507f2914d94
Cameron-Stock <clstock33@gmail.com>
2020-11-08 10:06:00 -0600

    finished profile config

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -20,6 +20,13 @@ namespace MethodsExercise
             Console.WriteLine($"{userName}, what is your favorite band?");
             var band = Console.ReadLine();
 
+            Console.WriteLine($"Thanks, {userName}! Here is your profile!");
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Band: {band}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
         }
     }
 }
