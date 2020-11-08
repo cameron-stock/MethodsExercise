9cae705f3329a6c408828be001973217719a2a5f
Cameron-Stock <clstock33@gmail.com>
2020-11-08 09:54:54 -0600

    began user questions

---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+
+            Console.WriteLine("Hello, what is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{userName}, what is your favorite band?");
+            var band = Console.ReadLine();
+
+        }
+    }
+}
